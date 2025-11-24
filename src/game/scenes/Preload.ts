7a5e27cf378170aabba3df734a1ed5112fcd9160
cuong <loutrinh2312000@gmail.com>
2025-11-24 15:01:53 +0700

    refactoring

--- src/game/scenes/Preload.ts
+++ src/game/scenes/Preload.ts
@@ -16,6 +16,7 @@ export class Preload extends Scene{
         this.preloadPurchase();
         this.preloadHeroRecruit();
         this.preloadBackBtn();
+        this.preloadInventory();
     }
     
     create() {
@@ -25,55 +26,7 @@ export class Preload extends Scene{
     preloadCommon() {
         this.load.setPath('assets');
         this.load.image('background', 'home.png');
-        this.load.image('black-bar', 'black-bar.png');
-        this.load.image('blue-bar-small', 'blue-bar-small.png');
-        this.load.image('broken-line', 'broken-line.png');
-        this.load.image('change-character', 'change-character.png');
-        this.load.image('character', 'character.png');
-        this.load.image('character-showcase-title', 'character-showcase-title.png');
-        this.load.image('character-pieces', 'character-pieces.png');
-        this.load.image('circle1', 'circle1.png');
-        this.load.image('circle2', 'circle2.png');
-        this.load.image('circle3', 'circle3.png');
-        this.load.image('claim-button', 'claim-button.png');
-        this.load.image('coin-1', 'coin-1.png');
-        this.load.image('coin-2', 'coin-2.png');
-        this.load.image('cost-chip', 'cost-chip.png');
-        this.load.image('dark-blue-bar', 'dark-blue-bar.png');
-        this.load.image('equip', 'equip.png');
-        this.load.image('extract', 'extract.png');
-        this.load.image('frame-achievement', 'frame-achievement.png');
-        this.load.image('frame-notice', 'frame-notice.png');
         this.load.image('frame-star', 'frame-star.png');
-        this.load.image('gift', 'gift.png');
-        this.load.image('go-button', 'go-button.png');
-        this.load.image('guild', 'guild.png');
-        this.load.image('gunner', 'gunner.png');
-        this.load.image('icon-copy', 'icon-copy.png');
-        this.load.image('icon-dark-bar-1', 'icon-dark-bar-1.png');
-        this.load.image('icon-dark-bar-11', 'icon-dark-bar-11.png');
-        this.load.image('icon-dark-bar-2', 'icon-dark-bar-2.png');
-        this.load.image('icon-dark-bar-22', 'icon-dark-bar-22.png');
-        this.load.image('icon-dark-bar-3', 'icon-dark-bar-3.png');
-        this.load.image('icon-dark-bar-4', 'icon-dark-bar-4.png');
-        this.load.image('icon-dark-bar-5', 'icon-dark-bar-5.png');
-        this.load.image('icon-gun', 'icon-gun.png');
-        this.load.image('id-frame', 'id-frame.png');
-        this.load.image('inventory', 'inventory.png');
-        this.load.image('inventory-title', 'inventory-title.png');
-        this.load.image('item-character-box', 'item-character-box.png');
-        this.load.image('item-info', 'item-info.png');
-        this.load.image('items', 'items.png');
-        this.load.image('level-10', 'level-10.png');
-        this.load.image('level-character', 'level-character.png');
-        this.load.image('level-up', 'level-up.png');
-        this.load.image('lib-icon', 'lib-icon.png');
-        this.load.image('max', 'max.png');
-        this.load.image('min', 'min.png');
-        this.load.image('minus', 'minus.png');
-        this.load.image('mint', 'mint.png');
-        this.load.image('mission', 'mission.png');
-        this.load.image('mission', 'mission.png');
         this.load.image('name-character-frame', 'name-character-frame.png');
         this.load.image('notice-icon', 'notice-icon.png');
         this.load.image('price-coin-frame', 'price-coin-frame.png');
@@ -127,8 +80,15 @@ export class Preload extends Scene{
         this.load.image('background-purchase', 'background-purchase.png');
         this.load.image('background-item', 'background-item.png');
         this.load.image('item-not-sale', 'item-not-sale.png');
-        this.load.image('item-purchase', 'item-purchase.png');
         this.load.image('item-sale', 'item-sale.png');
+        this.load.image('dark-blue-bar', 'dark-blue-bar.png');
+        this.load.image('icon-dark-bar-1', 'icon-dark-bar-1.png');
+        this.load.image('icon-dark-bar-11', 'icon-dark-bar-11.png');
+        this.load.image('icon-dark-bar-2', 'icon-dark-bar-2.png');
+        this.load.image('icon-dark-bar-22', 'icon-dark-bar-22.png');
+        this.load.image('icon-dark-bar-3', 'icon-dark-bar-3.png');
+        this.load.image('icon-dark-bar-4', 'icon-dark-bar-4.png');
+        this.load.image('icon-dark-bar-5', 'icon-dark-bar-5.png');
         this.load.setPath('');
     }
     
@@ -145,9 +105,21 @@ export class Preload extends Scene{
 
         //Bottom buttons
         this.load.image('blue-bar', 'blue-bar.png');
+        this.load.image('guild', 'guild.png');
+        this.load.image('inventory', 'inventory.png');
 
         //Top bar
         this.load.image('avatar-player', 'avatar-player.png');
+        this.load.image('black-bar', 'black-bar.png');
+        this.load.image('blue-bar-small', 'blue-bar-small.png');
+        this.load.image('coin-1', 'coin-1.png');
+        this.load.image('coin-2', 'coin-2.png');
+        this.load.image('icon-copy', 'icon-copy.png');
+        this.load.image('id-frame', 'id-frame.png');
+        
+        //Notice bar
+        this.load.image('gift', 'gift.png');
+        this.load.image('lib-icon', 'lib-icon.png');
         
         this.load.setPath('');
         
@@ -163,18 +135,50 @@ export class Preload extends Scene{
         this.load.setPath('assets/reward');
         this.load.image('achievement', 'achievement.png');
         this.load.image('airdrop', 'airdrop.png');
+        this.load.image('broken-line', 'broken-line.png');
+        this.load.image('claim-button', 'claim-button.png');
+        this.load.image('frame-achievement', 'frame-achievement.png');
+        this.load.image('go-button', 'go-button.png');
+        this.load.image('items', 'items.png');
+        this.load.image('mission', 'mission.png');
         this.load.setPath('');
     }
     
     preloadPurchase() {
         this.load.setPath('assets/purchase');
         this.load.image('add', 'add.png');
+        this.load.image('minus', 'minus.png');
+        this.load.image('cost-chip', 'cost-chip.png');
+        this.load.image('item-character-box', 'item-character-box.png');
+        this.load.image('item-info', 'item-info.png');
+        this.load.image('item-purchase', 'item-purchase.png');
+        this.load.image('max', 'max.png');
+        this.load.image('min', 'min.png');
         this.load.setPath('');
     }
     
     preloadHeroRecruit() {
         this.load.setPath('assets/hero-recruit');
         this.load.image('akane', 'akane.png');
+        this.load.image('character-showcase-title', 'character-showcase-title.png');
+        this.load.image('circle1', 'circle1.png');
+        this.load.image('circle2', 'circle2.png');
+        this.load.image('circle3', 'circle3.png');
+        this.load.image('equip', 'equip.png');
+        this.load.image('extract', 'extract.png');
+        this.load.image('gunner', 'gunner.png');
+        this.load.image('icon-gun', 'icon-gun.png');
+        this.load.image('level-10', 'level-10.png');
+        this.load.image('level-character', 'level-character.png');
+        this.load.image('level-up', 'level-up.png');
+        this.load.image('mint', 'mint.png');
+        this.load.setPath('');
+    }
+    
+    preloadInventory() {
+        this.load.setPath('assets/inventory');
+        this.load.image('character-pieces', 'character-pieces.png');
+        this.load.image('inventory-title', 'inventory-title.png');
         this.load.setPath('');
     }
 }
